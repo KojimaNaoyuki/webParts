35d478d28c12a67979064319bd06e0c8cea2d006
KojimaNaoyuki <programkoji213@gmail.com>
2021-02-22 19:11:37 +0900

    2021/02/22/4

--- scripts/libs/mobile-menu.js
+++ scripts/libs/mobile-menu.js
@@ -4,11 +4,15 @@ class MobileMenu {
         this.DOM = {}; //DOM要素を格納するためのオブジェクト
         this.DOM.btn = document.querySelector('.drawer-meny-icon-burger');
         this.DOM.head = document.querySelector('.header');
+        this.DOM.global = document.querySelector('.global-container');
+        this.DOM.mobileMenuList = document.querySelector('.mobile-menu-container');
         this._addEvent(); //イベントを付与
     }
 
     _toggle() {
         this.DOM.head.classList.toggle('drawer-meny-icon-burger-open');
+        this.DOM.global.classList.toggle('mobile-open');
+        this.DOM.mobileMenuList.classList.toggle('mobile-open');
     }
 
     _addEvent() {
