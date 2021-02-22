9532344cd55d525176ce459ccb43d545286b400e
KojimaNaoyuki <programkoji213@gmail.com>
2021-02-23 00:27:36 +0900

    2021/02/23/1

--- scripts/libs/mobile-menu.js
+++ scripts/libs/mobile-menu.js
@@ -1,4 +1,4 @@
-class MobileMenu {
+export class MobileMenu {
     constructor() {
         console.log('class MobileMenu');
         this.DOM = {}; //DOM要素を格納するためのオブジェクト
@@ -6,6 +6,7 @@ class MobileMenu {
         this.DOM.head = document.querySelector('.header');
         this.DOM.global = document.querySelector('.global-container');
         this.DOM.mobileMenuList = document.querySelector('.mobile-menu-container');
+        this.DOM.menuListContant = document.querySelectorAll('.menu-list');
         this._addEvent(); //イベントを付与
     }
 
@@ -18,5 +19,10 @@ class MobileMenu {
     _addEvent() {
         //dom要素にイベントを付与
         this.DOM.btn.addEventListener('click', this._toggle.bind(this));
+        
+        //メニューのリスト一つ一つにイベントを付与
+        this.DOM.menuListContant.forEach(element => {
+            element.addEventListener('click', this._toggle.bind(this));
+        });
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+import { MobileMenu } from './libs/mobile-menu.js';
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('main.js');
-    
+
     new MobileMenu();
 });
